4d33da196d2833438aa344149c1fdaf9cb44dee6
Anton <antonio.smilin@gmail.com>
2023-09-21 21:47:17 +0300

    start making homework module 21

--- app/src/main/java/com/tonykuz/findfilm/MainActivity.kt
+++ app/src/main/java/com/tonykuz/findfilm/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.tonykuz.findfilm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
+    fun onClickToast1(view: View) {
+        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+    fun onClickToast2(view: View) {
+        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast3(view: View) {
+        Toast.makeText(this, "Буду смотреть", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast4(view: View) {
+        Toast.makeText(this, "Профиль", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast5(view: View) {
+        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+    }
+    }

5833aceb27fc7d299dc69328fb7636d2cad0abc3
Anton <antonio.smilin@gmail.com>
2024-01-21 22:00:32 +0300

    homework module 37

---
+++ app/src/main/java/com/tonykuz/findfilm/di/modules/DatabaseModule.kt
@@ -0,0 +1,13 @@
+package com.tonykuz.findfilm.di.modules
+
+import com.tonykuz.findfilm.data.MainRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRepository() = MainRepository()
+}
--- app/src/main/java/com/tonykuz/findfilm/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/tonykuz/findfilm/viewmodel/HomeFragmentViewModel.kt
@@ -2,17 +2,20 @@ package com.tonykuz.findfilm.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tonykuz.findfilm.App
 import com.tonykuz.findfilm.domain.Film
 import com.tonykuz.findfilm.domain.Interactor
-import org.koin.core.KoinComponent
-import org.koin.core.inject
+import javax.inject.Inject
 
-class HomeFragmentViewModel : ViewModel(), KoinComponent {
+
+class HomeFragmentViewModel : ViewModel() {
     val filmsListLiveData:  MutableLiveData<List<Film>> = MutableLiveData()
     //Инициализируем интерактор
-    private val interactor: Interactor by inject()
+    @Inject
+    lateinit var interactor: Interactor
 
     init {
+        App.instance.dagger.inject(this)
         interactor.getFilmsFromApi(1, object : ApiCallback {
             override fun onSuccess(films: List<Film>) {
                 filmsListLiveData.postValue(films)

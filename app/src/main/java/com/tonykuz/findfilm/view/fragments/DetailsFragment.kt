785b39513fd935626ade7f430b6424b41b2b8265
Anton <antonio.smilin@gmail.com>
2024-02-21 22:20:32 +0300

    homework_module_42 - not working

--- app/src/main/java/com/tonykuz/findfilm/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/tonykuz/findfilm/view/fragments/DetailsFragment.kt
@@ -1,21 +1,37 @@
 package com.tonykuz.findfilm.view.fragments
 
+import android.Manifest
+import android.content.ContentValues
 import android.os.Bundle
 import com.tonykuz.findfilm.databinding.FragmentDetailsBinding
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+//import androidx.fragment.app.viewModels
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.tonykuz.findfilm.data.Entity.Film
 import com.tonykuz.findfilm.R
 import com.tonykuz.findfilm.data.ApiConstants
+import com.tonykuz.findfilm.viewmodel.DetailsFragmentViewModel
+import kotlinx.coroutines.*
 
 class DetailsFragment : Fragment() {
     //private var detailBinding: FragmentDetailsBinding? = null
     private lateinit var binding: FragmentDetailsBinding
     private lateinit var film: Film
+    private lateinit var viewModel: DetailsFragmentViewModel
+    //private val viewModel: DetailsFragmentViewModel by viewModels()
+    private val scope = CoroutineScope(Dispatchers.IO)
 
 
     override fun onCreateView(
@@ -57,8 +73,15 @@ class DetailsFragment : Fragment() {
             //Запускаем наше активити
             startActivity(Intent.createChooser(intent, "Share To:"))
         }
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
+        }
     }
 
+    //override fun onDestroy() {
+        //super.onDestroy()
+        //scope.cancel()
+    //}
 
     private fun setFilmsDetails() {
         //Получаем наш фильм из переданного бандла
@@ -79,5 +102,109 @@ class DetailsFragment : Fragment() {
             if (film.isInFavorites) R.drawable.baseline_favorite_24
             else R.drawable.baseline_favorite_border_24
         )
+    }
+
+    private fun performAsyncLoadOfPoster() {
+        //Проверяем есть ли разрешение
+        if (!checkPermission()) {
+            //Если нет, то запрашиваем и выходим из метода
+            requestPermission()
+            return
+        }
+        //Создаем родительский скоуп с диспатчером Main потока, так как будем взаимодействовать с UI
+        MainScope().launch {
+            //Включаем Прогресс-бар
+            binding.progressBar.isVisible = true
+            //Создаем через async, так как нам нужен результат от работы, то есть Bitmap
+            val job = scope.async {
+                viewModel.loadWallpaper(ApiConstants.IMAGES_URL + "original" + film.poster)
+            }
+            //Сохраняем в галерею, как только файл загрузится
+            saveToGallery(job.await())
+            //Выводим снекбар с кнопкой перейти в галерею
+            Snackbar.make(
+                binding.root,
+                R.string.downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+
+            //Отключаем Прогресс-бар
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    //Узнаем, было ли получено разрешение ранее
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    //Запрашиваем разрешение
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        //Проверяем версию системы
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Создаем объект для передачи данных
+            val contentValues = ContentValues().apply {
+                //Составляем информацию для файла (имя, тип, дата создания, куда сохранять и т.д.)
+                put(MediaStore.Images.Media.TITLE, film.title.handleSingleQuote())
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    film.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.Images.Media.DATE_ADDED,
+                    System.currentTimeMillis() / 1000
+                )
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/FilmsSearchApp")
+            }
+            //Получаем ссылку на объект Content resolver, который помогает передавать информацию из приложения вовне
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            //Открываем канал для записи на диск
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            //Передаем нашу картинку, может сделать компрессию
+            if (outputStream != null) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            }
+            //Закрываем поток
+            outputStream?.close()
+        } else {
+            //То же, но для более старых версий ОС
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                film.title.handleSingleQuote(),
+                film.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
     }
 }
